c881041d6144aae747b828d11cd253297a2d6447
jbaxx <jbaxx@users.noreply.github.com>
2021-02-21 21:34:47 -0500

    Implement GetRaw method for Album service
    - Implements a method to get raw bytes instead of json decoded
    - Implements test for the GetRaw method

--- deezer/album_test.go
+++ deezer/album_test.go
@@ -1,6 +1,7 @@
 package deezer
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -24,6 +25,27 @@ func TestGetAlbum(t *testing.T) {
 	want := &Album{ID: 44132881}
 	if !reflect.DeepEqual(album, want) {
 		t.Errorf("Album.Get got: %#v, want %#v", album, want)
+	}
+
+}
+
+func TestGetAlbumRaw(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/album/", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `{"id": 44132881}`)
+	})
+
+	albumRaw, _, err := client.Albums.GetRaw(44132881)
+	if err != nil {
+		t.Errorf("Album.Get return error: %v", err)
+	}
+
+	want := []byte(`{"id": 44132881}`)
+	if !bytes.Equal(albumRaw, want) {
+		t.Errorf("Album.Get got: %#v, want %#v", albumRaw, want)
 	}
 
 }

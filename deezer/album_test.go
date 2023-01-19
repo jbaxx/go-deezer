08dc4dcb851e0a67a6bdd252125da9528645a076
jbaxx <jbaxx@users.noreply.github.com>
2023-01-19 15:24:37 -0500

    Cleanup album tests

--- deezer/album_test.go
+++ deezer/album_test.go
@@ -1,7 +1,6 @@
 package deezer
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -27,44 +26,9 @@ func TestGetAlbum(t *testing.T) {
 		t.Errorf("Album.Get got: %#v, want %#v", album, want)
 	}
 
-}
-
-func TestGetAlbumRaw(t *testing.T) {
-	client, mux, teardown := setup()
-	defer teardown()
-
-	mux.HandleFunc("/album/", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, http.MethodGet)
-		fmt.Fprintf(w, `{"id": 44132881}`)
-	})
-
-	albumRaw, _, err := client.Albums.GetRaw("44132881")
-	if err != nil {
-		t.Errorf("Album.Get return error: %v", err)
-	}
-
-	want := []byte(`{"id": 44132881}`)
-	if !bytes.Equal(albumRaw, want) {
-		t.Errorf("Album.GetRaw got: %#v, want %#v", albumRaw, want)
-	}
-
-	_, _, err = client.Albums.GetRaw("\n")
-
+	_, _, err = client.Albums.Get("\n")
 	if err == nil {
 		t.Errorf("bad options err = nil, want error")
 	}
-
-	// client.URL.Path = ""
-	// f := func() (*Response, error) {
-	// 	got, resp, err := client.Albums.GetRaw(44132881)
-	// 	if got != nil {
-	// 		t.Errorf("want nil, got: %v", got)
-	// 	}
-	// 	return resp, err
-	// }
-	// resp, err := f()
-	// if resp != nil {
-	// 	t.Errorf("client.URL.Path='' resp = %#v, want nil", resp)
-	// }
 
 }
